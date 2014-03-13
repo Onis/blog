d54e03c3c5dfcea07be65729ba0fc8a4f55a0477
Onis <damir.s@mail.ru>
2014-03-13 21:37:14 +0600

    Создал модель комментариев и связал ее с публикациями

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,27 @@ class PostsController < ApplicationController
   	end
   end
 
+  def update
+  	@post = Post.find(params[:id])
+
+  	if @post.update(post_params)
+  		redirect_to @post
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+  	@post = Post.find(params[:id])
+  	@post.destroy
+
+  	redirect_to posts_path
+  end
+
+  def edit
+  	@post = Post.find(params[:id])
+  end
+
   def show
   	@post = Post.find(params[:id])
   end
